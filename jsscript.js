16a7c5251fa126bba2b8f9a15fde6e7ad894dcc8
guyNattawat <54878374+guyNattawat@users.noreply.github.com>
2021-10-18 21:55:13 +0700

    update web ver 0.1
    update เพิ่มหน้า section เพิ่มเติม ปรับปรุงในเรื่องของการ snap-scroll เเละ เรื่องการทำ font-size ด้วยนิดหน่อย

--- jsscript.js
+++ jsscript.js
@@ -1,4 +1,6 @@
 function onscrollFunction() {
     let newY = window.scrollY;
     document.body.style.setProperty("--op", newY * 0.3);
+    document.querySelector("#yyy").innerText = scrollY;
 };
+
