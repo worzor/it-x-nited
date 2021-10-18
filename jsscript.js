8729faae7930030162044ebcf5fe01bf1e506480
guyNattawat <54878374+guyNattawat@users.noreply.github.com>
2021-10-19 02:49:08 +0700

    fix nav-bar and update image.. js.. html
    Fix : nav-bar
    Update : ลด resolution บางรูปลงเพื่อให้เว็บโหลดได้เร็วมากยิ่งขึ้น
    เพิ่ม section เพิ่มเติมลงไป (--หน้าลุงคนที่ 1 ไม่ได้ทำตาม ref ต้องย่อลงมาเนื่องจาก รูปก็ 100vh เเล้วทำให้ใส่ตัวหนังสืออีกไม่ได้ เลยต้องบีบให้เข้ามาอยู่ในรูปเเทน)

--- jsscript.js
+++ jsscript.js
@@ -3,13 +3,21 @@ function onscrollFunction() {
     let newY = window.scrollY;
     document.body.style.setProperty("--op", newY * 0.3);
     document.querySelector("#yyy").innerText = scrollY;
+    
+    if(newY >= 1224){
+        document.getElementById("txt-p1").className = "txt-highlight-3";
+    }if(newY >= 4161){
+        document.getElementById("txt-p3").className = "txt-highlight-3";
+        document.getElementById("txt-p3-2").className = "txt-highlight-3";
+    }
 };
 
-function loading_Function() {
+
+/*function loading_Function() {
     myVar = setTimeout(showPage, 3000);
   }
   
 function showPage() {
     document.getElementById("loading-sc").style.display = "none";
     document.getElementById("site-body").style.display = "block";
-}
+}*/
