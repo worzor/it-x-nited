3dc24889127da9ecd0484b6e6a7118d5e20140b4
guyNattawat <54878374+guyNattawat@users.noreply.github.com>
2021-10-18 22:46:33 +0700

    update loading screen
    update ส่วนของ loading screen

--- jsscript.js
+++ jsscript.js
@@ -1,6 +1,15 @@
+var load_var
 function onscrollFunction() {
     let newY = window.scrollY;
     document.body.style.setProperty("--op", newY * 0.3);
     document.querySelector("#yyy").innerText = scrollY;
 };
 
+function loading_Function() {
+    myVar = setTimeout(showPage, 3000);
+  }
+  
+function showPage() {
+    document.getElementById("loading-sc").style.display = "none";
+    document.getElementById("site-body").style.display = "block";
+}
