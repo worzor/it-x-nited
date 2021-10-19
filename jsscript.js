f1ae7dd6382657e631dfd9a3bd0b5b260a1d2248
guyNattawat <54878374+guyNattawat@users.noreply.github.com>
2021-10-20 02:07:46 +0700

    update web first-page
    update เพิ่ม content
    เพิ่ม item เเละ animation
    ทำการ resize รูปบางรูป เพื่อให้ง่ายต่อการเขียน

--- jsscript.js
+++ jsscript.js
@@ -4,8 +4,15 @@ function onscrollFunction() {
     document.body.style.setProperty("--op", newY * 0.3);
     document.querySelector("#yyy").innerText = scrollY;
     
-    if(newY >= 1139){ /*ขอบบน 1224*/
-        document.getElementById("txt-p1").className = "txt-highlight-3"; 
+    if(newY >= 1000 && newY <= 1149){ /*ขอบบน 1224*/
+        var sec_y = newY - 1000;
+        document.getElementById("txt-p1").className = "txt-highlight-3";
+        document.body.style.setProperty("--scale", sec_y * 0.007);
+        /*trigger("section-nm-1", "#item");
+        trigger("section-nm-1", "#circle");*/
+    }if(newY >= 2000 && newY <= 2222){
+        var sec_y2 = newY - 2000;
+        document.body.style.setProperty("--scale_2", sec_y2 * 0.005);
     }if(newY >= 3945){ /*ขอบบน 4161*/ 
         document.getElementById("txt-p3").className = "txt-highlight-3";
     }if(newY >= 4390){
@@ -18,9 +25,43 @@ function onscrollFunction() {
         document.getElementById("txt-p5").className = "txt-highlight-3";
         document.getElementById("txt-p5-2").className = "txt-highlight-3";
         document.getElementById("img-gscale-p5").className = "img-nr-2 img-gscale-active";
+    }if(newY >= 7647){
+        document.getElementById("txt-p6").className = "txt-highlight-3";
+    }if(newY >= 8767){
+        document.getElementById("txt-p7").className = "txt-highlight-3";
+    }if(newY >= 9900){
+        document.getElementById("txt-p8").className = "txt-highlight-3";
+    }if(newY >= 10652){
+        time_show(newY, 10652);
+    }if(newY >= 10950){
+        document.getElementById("txt-p9").className = "txt-highlight-3";
+        document.getElementById("txt-p9-2").className = "txt-highlight-3";
+    }if(newY >= 12201){
+        document.getElementById("txt-p10").className = "txt-highlight-3";
     }
 };
 
+var count = 0;
+var sum = 0;
+function time_show(axis_y, y){
+    var time_all = document.getElementById("time-div").querySelectorAll("span#time");
+    if(count == 0){
+        sum = y;
+    }if(axis_y > sum && count < 11){
+        sum = sum + 50;
+        time_all[count].className = "time-active"; 
+        count++;
+    }
+}
+
+/*var head_id = ""
+var parent_id = ""
+function trigger(head_id, parent_id){
+    var time_all = document.getElementById(head_id).querySelectorAll(parent_id);
+    for(let i = 0; i < time_all.length; i++){
+        time_all[i].classList.add("active");
+    }
+}*/
 
 /*function loading_Function() {
     myVar = setTimeout(showPage, 3000);
