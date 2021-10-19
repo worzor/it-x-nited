ee66fa7a49b31e56988dc83b8ab29c822bd04810
guyNattawat <54878374+guyNattawat@users.noreply.github.com>
2021-10-19 15:00:26 +0700

    update เพิ่ม section fix scroll-snap
    update content เพิ่ม
    เอา scroll-snap ออก

--- jsscript.js
+++ jsscript.js
@@ -4,11 +4,20 @@ function onscrollFunction() {
     document.body.style.setProperty("--op", newY * 0.3);
     document.querySelector("#yyy").innerText = scrollY;
     
-    if(newY >= 1224){
-        document.getElementById("txt-p1").className = "txt-highlight-3";
-    }if(newY >= 4161){
+    if(newY >= 1139){ /*ขอบบน 1224*/
+        document.getElementById("txt-p1").className = "txt-highlight-3"; 
+    }if(newY >= 3945){ /*ขอบบน 4161*/ 
         document.getElementById("txt-p3").className = "txt-highlight-3";
+    }if(newY >= 4390){
         document.getElementById("txt-p3-2").className = "txt-highlight-3";
+    }if(newY >= 5608){
+        document.getElementById("txt-p4").className = "txt-highlight-3";
+        document.getElementById("txt-p4-2").className = "txt-highlight-3";
+        document.getElementById("img-gscale-p4").className = "img-nr img-gscale-active";
+    }if(newY >= 6666){
+        document.getElementById("txt-p5").className = "txt-highlight-3";
+        document.getElementById("txt-p5-2").className = "txt-highlight-3";
+        document.getElementById("img-gscale-p5").className = "img-nr-2 img-gscale-active";
     }
 };
 
