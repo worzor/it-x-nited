d6a6ae1d7f26f7773d280eb480e3946c4ae369f9
guyNattawat <54878374+guyNattawat@users.noreply.github.com>
2021-11-22 22:40:29 +0700

    Update Parallax

--- mainpage.js
+++ mainpage.js
@@ -1,26 +1,21 @@
-function onmove() {
-    document.addEventListener("mousemove", parallax);
-    const elem = document.querySelector(".parallax");
-    function parallax(e) {
-        let _w = window.innerWidth / 2;
-        let _h = window.innerHeight / 2;
-        let _mouseX = e.clientX;
-        let _mouseY = e.clientY;
-        let _depth1 = `${50 - (_mouseX - _w) * 0.005}% ${50 - (_mouseY - _h) * 0.005}%`;
-        let _depth2 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
-        let _depth3 = `${50 - (_mouseX - _w) * 0.03}% ${50 - (_mouseY - _h) * 0.03}%`;
+function parallax() {
+        const elem = document.querySelector(".parallax");
+        let _w = window.innerWidth;
+        let _h = window.innerHeight;
+        let newY = window.scrollY;
+
+        console.log(_h)
+        let _depth1 = `${100+(newY - _w) * 0.12}px ${40+(newY - _h) * 0.05}px`;
+        let _depth2 = `${50+(newY - _w) * 0.07}px ${(newY - _h) * 0.01}px`;
+        let _depth3 = `${(newY - _w) * 0.01}px`;
         let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+        console.log(x)
         elem.style.backgroundPosition = x;
-    }
-};
-
-function update(e){
-
-    var x = e.clientX || e.touches[0].clientX
-    var y = e.clientY || e.touches[0].clientY
-    document.documentElement.style.setProperty('--cursorX', x + 'px')
-    document.documentElement.style.setProperty('--cursorY', y + 'px')
 }
 
-document.addEventListener('mousemove',update)
-document.addEventListener('touchmove',update)
+function update(e){
+    let newY = window.scrollY;
+    document.documentElement.style.setProperty('--scroll', newY + 'px')
+}
+
+document.addEventListener('scroll',update)
